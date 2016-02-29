e46e5a92753ab15f120e1ef8db47ccb6d101c556
schleichardt <michael.schleichardt@commercetools.de>
2016-02-29 15:38:13 +0100

    improve credentials logic for release

--- src/test/java/com/commercetools/sdk/contrib/SphereClientRule.java
+++ src/test/java/com/commercetools/sdk/contrib/SphereClientRule.java
@@ -49,13 +49,15 @@ public final class SphereClientRule extends ExternalResource implements Blocking
 
     @Override
     protected void before() throws Throwable {
-        throw new RuntimeException("current dir " + new File(".").getAbsolutePath());
-//        try (final FileInputStream fileInputStream = new FileInputStream(new File("integrationtest.properties"))) {
-//            final Properties properties = new Properties();
-//            properties.load(fileInputStream);
-//            final SphereClientConfig config =  SphereClientConfig.ofProperties(properties, "");
-//            final SphereClient underlying = SphereClientFactory.of().createClient(config);
-//            client = BlockingSphereClient.of(underlying, 20, TimeUnit.SECONDS);
-//        }
+        final File file = new File(new File("integrationtest.properties").getAbsolutePath()
+                .replace("/target/checkout", "")//in case it is at release time
+        );
+        try (final FileInputStream fileInputStream = new FileInputStream(file)) {
+            final Properties properties = new Properties();
+            properties.load(fileInputStream);
+            final SphereClientConfig config =  SphereClientConfig.ofProperties(properties, "");
+            final SphereClient underlying = SphereClientFactory.of().createClient(config);
+            client = BlockingSphereClient.of(underlying, 20, TimeUnit.SECONDS);
+        }
     }
 }
