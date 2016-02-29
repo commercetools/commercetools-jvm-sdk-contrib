5652f38753ec37d3db76c94315295904861936b4
schleichardt <michael.schleichardt@commercetools.de>
2016-02-29 16:13:08 +0100

    improve credentials logic for release

--- src/test/java/com/commercetools/sdk/contrib/SphereClientRule.java
+++ src/test/java/com/commercetools/sdk/contrib/SphereClientRule.java
@@ -49,26 +49,21 @@ public final class SphereClientRule extends ExternalResource implements Blocking
 
     @Override
     protected void before() throws Throwable {
-        final String debugOutput = "current dir "
-                + new File(".").getAbsolutePath()
-//              +  " " + new File(".").getParentFile().getParentFile().getAbsolutePath()
-                ;
-        throw new RuntimeException(debugOutput);
-
-//        File file = new File("integrationtest.properties");
-//        if (!file.exists()
-//                && new File(".").getParentFile().exists()
-//                && new File(".").getParentFile().getParentFile().exists()
-//                && new File(".").getParentFile().getParentFile().getParentFile().exists()
-//                && new File(new File(".").getParentFile().getParentFile().getParentFile(), "integrationtest.properties").exists()) {
-//            file = new File(new File(".").getParentFile().getParentFile().getParentFile(), "integrationtest.properties");
-//        }
-//        try (final FileInputStream fileInputStream = new FileInputStream(file)) {
-//            final Properties properties = new Properties();
-//            properties.load(fileInputStream);
-//            final SphereClientConfig config =  SphereClientConfig.ofProperties(properties, "");
-//            final SphereClient underlying = SphereClientFactory.of().createClient(config);
-//            client = BlockingSphereClient.of(underlying, 20, TimeUnit.SECONDS);
-//        }
+        File file = new File("integrationtest.properties");
+        final File parentFile = new File(".").getParentFile();
+        if (!file.exists()
+                && parentFile !=null && parentFile.exists()
+                && parentFile.getParentFile() != null && parentFile.getParentFile().exists()
+                && parentFile.getParentFile().getParentFile() != null && parentFile.getParentFile().getParentFile().exists()
+                && new File(parentFile.getParentFile().getParentFile(), "integrationtest.properties").exists()) {
+            file = new File(parentFile.getParentFile().getParentFile(), "integrationtest.properties");
+        }
+        try (final FileInputStream fileInputStream = new FileInputStream(file)) {
+            final Properties properties = new Properties();
+            properties.load(fileInputStream);
+            final SphereClientConfig config =  SphereClientConfig.ofProperties(properties, "");
+            final SphereClient underlying = SphereClientFactory.of().createClient(config);
+            client = BlockingSphereClient.of(underlying, 20, TimeUnit.SECONDS);
+        }
     }
 }
