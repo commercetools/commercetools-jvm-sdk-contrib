ea7b2a883fead5d43f4eebf37fa8e410f83a0c74
schleichardt <michael.schleichardt@commercetools.de>
2016-03-01 10:01:38 +0100

    allow test credentials via env args

--- src/test/java/com/commercetools/sdk/contrib/SphereClientRule.java
+++ src/test/java/com/commercetools/sdk/contrib/SphereClientRule.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 import java.util.concurrent.CompletionStage;
@@ -49,14 +50,31 @@ public final class SphereClientRule extends ExternalResource implements Blocking
 
     @Override
     protected void before() throws Throwable {
+        if (System.getenv("JVM_SDK_CONTRIB_IT_PROJECT_KEY") != null) {
+            initializeClientFromEnv();
+        } else {
+            initializeClientFromProperties();
+        }
+    }
+
+    private void initializeClientFromEnv() {
+        final SphereClientConfig config = SphereClientConfig.ofEnvironmentVariables("JVM_SDK_CONTRIB_IT");
+        initializeClient(config);
+    }
+
+    private void initializeClientFromProperties() throws IOException {
         final File file = findFile(new File("integrationtest.properties").getAbsoluteFile(), 5);
         try (final FileInputStream fileInputStream = new FileInputStream(file)) {
             final Properties properties = new Properties();
             properties.load(fileInputStream);
-            final SphereClientConfig config =  SphereClientConfig.ofProperties(properties, "");
-            final SphereClient underlying = SphereClientFactory.of().createClient(config);
-            client = BlockingSphereClient.of(underlying, 20, TimeUnit.SECONDS);
+            final SphereClientConfig config = SphereClientConfig.ofProperties(properties, "");
+            initializeClient(config);
         }
+    }
+
+    private void initializeClient(final SphereClientConfig config) {
+        final SphereClient underlying = SphereClientFactory.of().createClient(config);
+        client = BlockingSphereClient.of(underlying, 20, TimeUnit.SECONDS);
     }
 
     private static File findFile(final File initial, final int ttl) {
